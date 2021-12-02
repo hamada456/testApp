25e71527a3f5a5a428a8722b5018a4e48639041c
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-12-02 17:01:50 +0900

    メモを追記

--- app/src/test/java/com/example/testapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/testapp/ExampleUnitTest.kt
@@ -15,10 +15,12 @@ import org.junit.Assert.*
 //        return a + b
 //    }
 //}
+
 internal class CalcTest {
     @Test
-    fun addメソッドに2と3を渡すと5を返す() {
+    fun addメソッドに2と3を渡すと5を返すCalcクラスのテスト() {
         //テスト対象のCalcクラスをsutに代入、expected：期待される
+        //Calcクラスをpublicにしておく（値が読み出せずにエラーになる為）
         val sut = Calc()
         val expected = 5
         //addのaとbに数字を入れる、actual：実際
